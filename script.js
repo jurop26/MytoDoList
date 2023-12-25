a70b962c943b890ae361dda7fbe0623918292007
Juraj Packa <jurop26@azet.sk>
2023-12-25 15:01:02 +0100

    load, save from locastorage

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const form = document.querySelector('#new-task-form');
 const input = document.querySelector('#new-task-item');
 const button = document.querySelector('button');
 const list = document.querySelector('#list-todo');
-const todos = [];
+let tasks = loadTasks();
+tasks.forEach(addToList);
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log("add");
     if ((input === null || input === void 0 ? void 0 : input.value) == '' || (input === null || input === void 0 ? void 0 : input.value) == null)
         return;
     const newTask = {
@@ -14,20 +14,45 @@ form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) 
         complete: false,
         id: new Date().valueOf().toString()
     };
-    todos.push(newTask);
+    tasks.push(newTask);
     addToList(newTask);
+    saveTasks();
     input.value = '';
 });
 function addToList(task) {
     const li = document.createElement('li');
-    const label = document.createElement('label');
     const checkbox = document.createElement('input');
+    const label = document.createElement('label');
     checkbox.type = 'checkbox';
     checkbox.checked = task.complete;
+    checkbox.addEventListener('change', () => {
+        task.complete = checkbox.checked;
+        saveTasks();
+    });
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+    taskDiv.innerText = task.title;
+    taskDiv.dataset.id = task.id;
     const deleteButton = document.createElement('button');
     deleteButton.setAttribute('class', 'delete');
     deleteButton.innerText = 'delete';
-    label.append(checkbox, task.title, deleteButton);
+    deleteButton.addEventListener('click', (e) => {
+        var _a;
+        const target = e.target;
+        (_a = target === null || target === void 0 ? void 0 : target.closest('li')) === null || _a === void 0 ? void 0 : _a.remove();
+        tasks = tasks.filter(t => t.id !== task.id);
+        saveTasks();
+    });
+    label.append(checkbox, taskDiv, deleteButton);
     li.append(label);
     list === null || list === void 0 ? void 0 : list.append(li);
+}
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+function loadTasks() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks == null)
+        return [];
+    return JSON.parse(tasks);
 }
--- script.ts
+++ script.ts
@@ -9,11 +9,12 @@ const input = document.querySelector<HTMLInputElement>('#new-task-item')
 const button = document.querySelector<HTMLButtonElement>('button')
 const list = document.querySelector<HTMLUListElement>('#list-todo')
 
-const todos: Task[] = []
+let tasks: Task[] = loadTasks()
+tasks.forEach(addToList)
 
 form?.addEventListener('submit', (e): void => {
     e.preventDefault()
-console.log("add")
+
     if(input?.value == '' || input?.value == null) return
     
     const newTask = {
@@ -21,23 +22,51 @@ console.log("add")
         complete: false, 
         id: new Date().valueOf().toString()
     }
-    todos.push(newTask)
+    tasks.push(newTask)
     addToList(newTask)
+    saveTasks()
     input.value = ''
 })
 
+
 function addToList(task: Task): void{
     const li = document.createElement('li')
-    const label = document.createElement('label')
     const checkbox = document.createElement('input')
+    const label = document.createElement('label')
     checkbox.type = 'checkbox'
     checkbox.checked = task.complete
+    checkbox.addEventListener('change', (): void => {
+        task.complete = checkbox.checked
+        saveTasks()
+        })
+
+    const taskDiv = document.createElement('div')
+    taskDiv.classList.add('task')
+    taskDiv.innerText = task.title
+    taskDiv.dataset.id = task.id
+
     const deleteButton = document.createElement('button')
     deleteButton.setAttribute('class', 'delete')
     deleteButton.innerText = 'delete'
-
-    label.append(checkbox, task.title, deleteButton)
+    deleteButton.addEventListener('click', (e): void => {
+        const target = e.target as HTMLButtonElement
+        target?.closest('li')?.remove()
+        tasks = tasks.filter(t => t.id !== task.id)
+        
+        saveTasks()
+    })
+    
+    label.append(checkbox, taskDiv, deleteButton)
     li.append(label)
     list?.append(li)
 }
 
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const tasks = localStorage.getItem('tasks')
+    if(tasks == null) return []
+    return JSON.parse(tasks)
+}
